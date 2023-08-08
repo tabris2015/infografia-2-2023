3fbc1560629dea71592d20aff608e508c310a315
Jose Laruta <eduardo.laruta@gmail.com>
2023-08-08 08:58:52 -0400

    anim 1

--- 4.circle/main.py
+++ 4.circle/main.py
@@ -7,22 +7,39 @@ SCREEN_HEIGHT = 800
 SCREEN_TITLE = "Circulos con bresenham"
 
 
-
 class BresenhamWindow(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         arcade.set_background_color(arcade.color.BLACK)
-        self.pixel_size = 25
+        self.pixel_size = 5
+        self.xc = 50
+        self.yc = 50
+        self.r = 20
+        self.circle_color = arcade.color.RED_DEVIL
+
+        self.speed = 10
+
+    def on_update(self, delta_time: float):
+        self.xc += delta_time * self.speed
+        
+        print(self.xc)
+        if self.xc > SCREEN_WIDTH // self.pixel_size:
+            self.xc = 0
 
     def on_draw(self):
         arcade.start_render()
-        xc = 15
-        yc = 15
-        r = 15
-        points = get_circle(xc, yc, r)
+        points = get_circle(
+            self.xc,
+            self.yc,
+            self.r
+        )
         self.draw_grid()
         self.draw_circle_points(points, arcade.color.DARK_YELLOW)
-        self.draw_scaled_circle(xc, yc, r)
+        self.draw_scaled_circle(
+            self.xc,
+            self.yc,
+            self.r
+        )
 
     def draw_grid(self):
         # lineas verticales
