5b18aa364d08c9ed452857892eb4a5c1221a7949
Jose Laruta <eduardo.laruta@gmail.com>
2023-08-09 08:08:25 -0400

    translation

--- 5.transformations/main.py
+++ 5.transformations/main.py
@@ -1,4 +1,5 @@
 import arcade
+import numpy as np
 
 # definicion de constantes
 SCREEN_WIDTH = 800
@@ -15,7 +16,23 @@ class BresenhamWindow(arcade.Window):
         arcade.start_render()
         vertices = [(100, 100), (100, 200), (200, 200), (200, 100)]
         arcade.draw_polygon_outline(vertices, arcade.color.YELLOW, 5)
+        new_vertices = self.translate2(vertices, 200, 200)
+        arcade.draw_polygon_outline(new_vertices, arcade.color.CYBER_YELLOW, 5)
 
+    def translate(self, vertices, dx, dy):
+        new_vertices = [(v[0] + dx, v[1] + dy) for v in vertices]
+        return new_vertices
+    
+    def translate2(self, vertices, dx, dy):
+        TM = np.array([[1, 0, dx], [0, 1, dy], [0, 0, 1]])
+        v_array = np.array([[v[0], v[1], 1] for v in vertices])
+        v_array = np.transpose(v_array)
+        # aplicar transformacion
+        new_vertices_array = np.dot(TM, v_array)
+        new_vertices = np.transpose(new_vertices_array[0:2, :])
+        new_vertices = new_vertices.tolist()
+        print(new_vertices)
+        return new_vertices
 
 
 if __name__ == "__main__":
