55cea83e885615912703d4315c8cceda3f22b8f0
Jose Laruta <eduardo.laruta@gmail.com>
2023-08-09 08:59:20 -0400

    transformations

--- 5.transformations/main.py
+++ 5.transformations/main.py
@@ -16,25 +16,71 @@ class BresenhamWindow(arcade.Window):
         arcade.start_render()
         vertices = [(100, 100), (100, 200), (200, 200), (200, 100)]
         arcade.draw_polygon_outline(vertices, arcade.color.YELLOW, 5)
-        new_vertices = self.translate2(vertices, 200, 200)
+        new_vertices = self.scale(vertices, 2.5, 2.5)
         arcade.draw_polygon_outline(new_vertices, arcade.color.CYBER_YELLOW, 5)
-
-    def translate(self, vertices, dx, dy):
-        new_vertices = [(v[0] + dx, v[1] + dy) for v in vertices]
-        return new_vertices
     
-    def translate2(self, vertices, dx, dy):
+    def translate(self, vertices, dx, dy):
         TM = np.array([[1, 0, dx], [0, 1, dy], [0, 0, 1]])
+
+        return self.apply_tr_matrix(vertices, TM)
+
+    def rotate(self, vertices, theta):
+        xc, yc = self.get_center(vertices)
+        Mt1 = np.array([
+            [1, 0, -xc], 
+            [0, 1, -yc], 
+            [0, 0, 1]
+        ])
+        Mr = np.array([
+            [np.cos(theta), -np.sin(theta), 0], 
+            [np.sin(theta), np.cos(theta), 0], 
+            [0, 0, 1]
+        ])
+        Mt2 = np.array([
+            [1, 0, xc], 
+            [0, 1, yc], 
+            [0, 0, 1]
+        ])
+
+        TM = np.dot(Mt2, np.dot(Mr, Mt1))
+  
+        return self.apply_tr_matrix(vertices, TM)
+
+    def scale(self, vertices, sx, sy):
+        xc, yc = self.get_center(vertices)
+        Mt1 = np.array([
+            [1, 0, -xc], 
+            [0, 1, -yc], 
+            [0, 0, 1]
+        ])
+        Ms = np.array([
+            [sx, 0, 0], 
+            [0, sy, 0], 
+            [0, 0, 1]
+        ])
+        Mt2 = np.array([
+            [1, 0, xc], 
+            [0, 1, yc], 
+            [0, 0, 1]
+        ])
+
+        TM = np.dot(Mt2, np.dot(Ms, Mt1))
+  
+        return self.apply_tr_matrix(vertices, TM)
+
+
+    def apply_tr_matrix(self, vertices, tr_matrix):
         v_array = np.array([[v[0], v[1], 1] for v in vertices])
         v_array = np.transpose(v_array)
         # aplicar transformacion
-        new_vertices_array = np.dot(TM, v_array)
+        new_vertices_array = np.dot(tr_matrix, v_array)
         new_vertices = np.transpose(new_vertices_array[0:2, :])
         new_vertices = new_vertices.tolist()
-        print(new_vertices)
         return new_vertices
 
-
+    def get_center(self, vertices):
+        return np.mean(np.array(vertices), axis=0)
+    
 if __name__ == "__main__":
     app = BresenhamWindow()
     arcade.run()
