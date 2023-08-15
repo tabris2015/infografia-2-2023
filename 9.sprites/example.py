b0b288e9c4a92ef01b5e412b04dc84cd3b9276d3
Jose Laruta <eduardo.laruta@gmail.com>
2023-08-15 07:44:01 -0400

    sprites speed

--- 9.sprites/example.py
+++ 9.sprites/example.py
@@ -10,16 +10,28 @@ class App(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         arcade.set_background_color(arcade.color.BLACK)
-        self.player = arcade.Sprite("img/mario.png", 1, center_x=SCREEN_WIDTH / 2, center_y=SCREEN_HEIGHT / 2)
+        self.player = arcade.Sprite(
+            "9.sprites/img/mario.png", 
+            1, 
+            center_x=SCREEN_WIDTH / 2, 
+            center_y=SCREEN_HEIGHT / 2
+        )
+        self.player_speed = 0
+        
 
     def on_key_press(self, symbol: int, modifiers: int):
         if symbol == arcade.key.LEFT:
-            self.player.center_x += 5
+            self.player_speed = -55
         if symbol == arcade.key.RIGHT:
-            self.player.center_x -= 5
+            self.player_speed = 55
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        if symbol in (arcade.key.LEFT, arcade.key.RIGHT):
+            self.player_speed = 0
 
     def on_update(self, delta_time: float):
         """Metodo para actualizar objetos de la app"""
+        self.player.center_x += self.player_speed * delta_time
         self.player.update()
 
     def on_draw(self):
