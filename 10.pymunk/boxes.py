b048444b9f720693fb7f460afa1c5d05be0cfede
Jose Laruta <eduardo.laruta@gmail.com>
2023-08-17 07:45:24 -0400

    drag

--- 10.pymunk/boxes.py
+++ 10.pymunk/boxes.py
@@ -84,16 +84,32 @@ class App(arcade.Window):
             inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
             body = pymunk.Body(mass, inertia)
             body.position = (x, y)
-            body.velocity = 500, 0
+            body.velocity = 900, 0
             shape = pymunk.Circle(body, radius, (0, 0))
             shape.friction = 0.4
             self.space.add(body, shape)
 
             sprite = CircleSprite(shape, ":resources:images/items/coinGold.png")
             self.sprite_list.append(sprite)
+        if button == arcade.MOUSE_BUTTON_RIGHT:
+            self.last_mouse_position = x, y
+            shape_list = self.space.point_query((x, y), 1, pymunk.ShapeFilter())
+            if len(shape_list) > 0:
+                self.shape_being_dragged = shape_list[0]
+
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
+        if self.shape_being_dragged is not None:
+            self.last_mouse_position = (x, y)
+            self.shape_being_dragged.shape.body.position = self.last_mouse_position
+            self.shape_being_dragged.shape.body.velocity = (dx * 20, dy * 20)
+    
+    def on_mouse_release(self, x: int, y: int, button: int, modifiers: int):
+        if button == arcade.MOUSE_BUTTON_RIGHT:
+            self.shape_being_dragged = None
 
     def on_update(self, delta_time: float):
         self.space.step(1 / 60)
+        self.sprite_list.update()
         for sprite in self.sprite_list:
             sprite.center_x = sprite.pymunk_shape.body.position.x
             sprite.center_y = sprite.pymunk_shape.body.position.y
