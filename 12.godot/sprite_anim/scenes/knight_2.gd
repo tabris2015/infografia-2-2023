f186b27a735be56a187776afadfd32c367305d62
Jose Laruta <eduardo.laruta@gmail.com>
2023-08-28 09:12:44 -0400

    state machines

---
+++ 12.godot/sprite_anim/scenes/knight_2.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 80
+const FRICTION = 500
+var is_attacking = false
+var state_machine
+func _ready():
+	state_machine = $AnimationTree["parameters/playback"]
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		# movimiento
+		state_machine.travel("run")
+	else:
+		state_machine.travel("idle")
+		
+	if Input.is_action_just_pressed("attack"):
+		state_machine.travel("attack")
+		
+	print(input_vector)
+	velocity = MAX_SPEED * input_vector
+	# si mira a la izquierda o derecha
+	if velocity.x < 0:
+		$Sprite2D.scale.x = abs($Sprite2D.scale.x) * -1
+	elif velocity.x > 0:
+		$Sprite2D.scale.x = abs($Sprite2D.scale.x)
+	
+	move_and_slide()
