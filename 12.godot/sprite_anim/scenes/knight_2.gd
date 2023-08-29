4abec320c6a7c21bc9c4f517a0ce2fa29c5fe5af
Jose Laruta <eduardo.laruta@gmail.com>
2023-08-29 09:14:55 -0400

    hurtbox with timer

--- 12.godot/sprite_anim/scenes/knight_2.gd
+++ 12.godot/sprite_anim/scenes/knight_2.gd
@@ -6,6 +6,17 @@ const FRICTION = 500
 var hp = 5 # puntos de vitalidad
 var is_attacking = false
 var state_machine
+
+var is_hit = false
+var is_dead = false
+
+func take_damage(damage):
+	is_hit = true
+	hp -= damage
+	print("hp: ", hp)
+	if hp <= 0:
+		is_dead = true
+
 func _ready():
 	state_machine = $AnimationTree["parameters/playback"]
 
@@ -23,9 +34,19 @@ func _physics_process(delta):
 		
 	if Input.is_action_just_pressed("attack"):
 		state_machine.travel("attack")
-		
-	#print(input_vector)
+	
 	velocity = MAX_SPEED * input_vector
+	
+	if is_hit:
+		state_machine.travel("hit")
+		is_hit = false
+	if is_dead:
+		state_machine.travel("death")
+		$Timer.stop()
+		velocity = Vector2.ZERO
+	
+	#print(input_vector)
+	
 	# si mira a la izquierda o derecha
 	if velocity.x < 0:
 		$Sprite2D.scale.x = abs($Sprite2D.scale.x) * -1
@@ -35,6 +56,15 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _on_hurtbox_area_entered(area):
-	print("damage!")
-	hp -= 1
-	
+	take_damage(1)
+	$Timer.start()
+	$Timer.one_shot = false
+
+func _on_timer_timeout():
+	take_damage(1)
+	print("in damage")
+
+
+func _on_hurtbox_area_exited(area):
+	$Timer.stop()
+	$Timer.one_shot = true
